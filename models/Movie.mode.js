4631a88ed637eae2eef2c8add45e517d8e35f6de
Alberto Sclocchi <scloks75tv@gmail.com>
2025-06-03 19:00:12 -0400

    initialize project and create movie model

---
+++ models/Movie.mode.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const movieSchema = new Schema(
+  {
+    title: {
+      type: String
+    },
+    releaseYear: {
+      type: Number
+    },
+    overview: {
+      type: String
+    },
+    verticalPoster:{
+      type: String
+    },
+    rating:{
+        type: Number
+    },
+    cast:[{
+        type: String
+    }]
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Movie = model("Movie", movieSchema);
+
+module.exports = Movie;
