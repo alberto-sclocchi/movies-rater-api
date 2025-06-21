48035567c14bee1f8ab044873770998fc6606aad
Alberto Sclocchi <scloks75tv@gmail.com>
2025-06-21 19:58:09 -0400

    update user model and routes

--- models/User.model.js
+++ models/User.model.js
@@ -4,19 +4,20 @@ const userSchema = new Schema(
   {
     email: {
       type: String,
-      required: [true, "Email is required."],
-      unique: true,
-      lowercase: true,
-      trim: true,
     },
     password: {
       type: String,
-      required: [true, "Password is required."],
     },
     name: {
       type: String,
-      required: [true, "Name is required."],
     },
+    username: {
+      type: String,
+    },
+    moviesAdded: {
+      type: Schema.Types.ObjectId,
+      ref: "Movie"
+    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
